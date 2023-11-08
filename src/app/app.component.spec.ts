711fe66d7db9612872cc8486efe45beeee52084a
panaototvs <lucas.panao@totvs.com.br>
2023-11-08 13:46:52 -0300

    Fontes principais

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,62 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
 
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent],
+      imports: [RouterTestingModule.withRoutes([])],
+    });
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
 
-  it(`should have as title 'my-app'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-app');
+  it('deve ser instanciado', () => {
+    expect(component).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-app app is running!');
+  it('ngOnInit - deve iniciar o componente', () => {
+    const spy = spyOn(component,'getMoreItems');
+    component.ngOnInit()
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('joinStrings - deve unir duas strings', () => {
+    const result = component.joinStrings('Hello', 'World');
+    expect(result).toEqual('HelloWorld');
+  });
+
+  it('numStrBool - deve retornar o tipo da entrada', () => {
+    expect(component.numStrBool(42)).toEqual('number');
+    expect(component.numStrBool('Hello')).toEqual('string');
+    expect(component.numStrBool(true)).toEqual('boolean');
+  });
+
+  it('addTwoNumbers - deve somar dois números', () => {
+    const result1 = component.addTwoNumbers(5, 3);
+    const result2 = component.addTwoNumbers(5);
+    expect(result1).toEqual(8);
+    expect(result2).toEqual(5);
+  });
+
+  it('numToPow - deve elevar um número a uma potência', () => {
+    const result = component.numToPow(2, 3);
+    expect(result).toEqual(8);
+  });
+
+  describe('o método getMoreItems', () => {
+    it('deve retornar true se isShowMore for verdadeiro', () => {
+      const result = component.getMoreItems(true);
+      expect(result).toBeTrue();
+    });
+
+    it('deve retornar false se isShowMore for falso', () => {
+      const result = component.getMoreItems(false);
+      expect(result).toBeFalse();
+    });
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'my-app';
+
+  ngOnInit(): void {
+    this.getMoreItems(true);
+  }
+
+  joinStrings(str1: string, str2: string): string {
+    return str1 + str2
+  }
+
+  numStrBool(val: number | string | boolean) {
+    return typeof val
+  }
+
+  addTwoNumbers(num1: number, num2 = 0): number {
+    return num1 + num2
+  }
+
+  numToPow(num: number, exp: number): number {
+    return num ** exp
+  }
+
+  getMoreItems(isShowMore: boolean): boolean {
+    if (isShowMore) {
+      return true
+    } else {
+      return false
+    }
+  }
+
 }
